fa6c61638b5ec2f1f8f1205e107a964f841d8050
sqrkl <152407511+sqrkl@users.noreply.github.com>
2024-02-09 06:10:14 +1100

    MAGI metric

--- script.js
+++ script.js
@@ -5,6 +5,179 @@ document.addEventListener('DOMContentLoaded', function() {
 	applySystemTheme();
 });
 
+const leaderboardDataEqbench = `model,score,params
+NousResearch/Nous-Capybara-34B,68.47,34
+jondurbin/bagel-34b-v0.2,66.07,34
+internlm/internlm2-chat-7b,62.61,7
+NousResearch/Nous-Hermes-2-Yi-34B,72.68,34
+Yhyu13/LMCocktail-10.7B-v1,73.67,10.7
+01-ai/Yi-34B-Chat,71.62,34
+Open-Orca/Mistral-7B-OpenOrca,66.55,7
+fblgit/una-cybertron-7b-v2-bf16,62.83,7
+Intel/neural-chat-7b-v3-1,64.77,7
+Toten5/Marcoroni-neural-chat-7B-v2,68.54,7
+huggingfaceh4/zephyr-7b-beta,58.33,7
+madatnlp/marcoroni-7b-v3-safetensor,71.68,7
+mistralai/mistral-7b-instruct-v0.1,52.15,7
+Weyaxi/SauerkrautLM-UNA-SOLAR-Instruct,73.56,10.7
+huggingfaceh4/zephyr-7b-alpha,56.82,7
+meta-llama/Llama-2-13b-chat-hf,49.12,13
+zyh3826/GML-Mistral-merged-v1,74.01,7
+upstage/SOLAR-10.7B-Instruct-v1.0,73.53,10.7
+cognitivecomputations/dolphin-2_2-yi-34b,75.52,34
+cognitivecomputations/dolphin-2.2-70b,79.6,70
+gpt-4-0314,85.73,
+gpt-4-0613,84.79,
+gpt-4-1106-preview,86.05,
+TheBloke/koala-7B-HF,21.54,7
+meta-llama/Llama-2-70b-chat-hf,73.59,70
+lmsys/vicuna-7b-v1.1,26.12,7
+NousResearch/Nous-Capybara-7B-V1,34.37,7
+mistral-medium,82.57,
+meta-llama/Llama-2-7b-chat-hf,36.32,7
+gemini-pro,75.08,
+migtissera/SynthIA-70B-v1.5,73.71,70
+openchat/openchat-3.5-1210,72.52,7
+openchat/openchat_3.5,72.18,7
+mlabonne/Beagle14-7B,74.45,7
+mlabonne/NeuralMarcoro14-7B,74.15,7
+YeungNLP/firefly-mixtral-8x7b,64.36,8x7
+mlabonne/NeuralHermes-2.5-Mistral-7B,65.86,7
+cloudyu/Mixtral_34Bx2_MoE_60B,72.69,34x2
+mistralai/Mixtral-8x7B-Instruct-v0.1,72.37,8x7
+mistralai/Mistral-7B-Instruct-v0.2,68.18,7
+lxuechen/phi-2-dpo,54.42,2.7
+rhysjones/phi-2-orange,56.94,2.7
+mlabonne/phixtral-2x2_8,54.58,2x2.7
+microsoft/phi-2,27.6,2.7
+mlabonne/Beyonder-4x7B-v2,69.23,4x7
+gpt-3.5-turbo-1106,71.74,
+gpt-3.5-turbo-0613,69.35,
+gpt-3.5-turbo-0301,70.67,
+rishiraj/meow,73.94,10.7
+jondurbin/nontoxic-bagel-34b-v0.2,70.21,34
+alpindale/goliath-120b,76.09,120
+migtissera/Tess-XL-v1.0,78.46,120
+mlabonne/NeuralBeagle14-7B,74.79,7
+NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT,72.91,8x7
+vince62s/phi-2-psy,56.44,2.7
+stabilityai/stablelm-2-zephyr-1_6b,15.04,1.6
+cognitivecomputations/MegaDolphin-120b,80.21,120
+OrionStarAI/Orion-14B-Chat,59.71,14
+cognitivecomputations/laserxtral,71.96,4x7
+deepseek-ai/deepseek-llm-67b-chat,77.53,67
+macadeliccc/SOLAR-10.7b-Instruct-dpo,73.21,10.7
+tiiuae/falcon-180B-chat,56.82,180
+Qwen/Qwen-1_8B-Chat,30,1.8
+Qwen/Qwen-14B-Chat,63.47,14
+Qwen/Qwen-7B-Chat,50.11,7
+01-ai/Yi-6B-Chat,61.79,6
+miqudev/miqu-1-70b,82.91,70
+DiscoResearch/DiscoLM-120b,78.48,120
+Qwen/Qwen-72B-Chat,80.7,72
+WizardLM/WizardLM-70B-V1.0,71.28,70
+lmsys/vicuna-13b-v1.5,67.39,13
+allenai/tulu-2-dpo-70b,76.63,70
+WizardLM/WizardLM-13B-V1.2,63.71,13
+cognitivecomputations/dolphin-2.2.1-mistral-7b,69.92,7
+timdettmers/guanaco-33b-merged,36.11,33
+teknium/OpenHermes-2.5-Mistral-7B,66.89,7
+berkeley-nest/Starling-LM-7B-alpha,73.9,7
+lmsys/vicuna-33b-v1.3,67.07,33
+serpdotai/sparsetral-16x7B-v2,59.9,9
+Qwen/Qwen1.5-14B-Chat,74.99,14
+Qwen/Qwen1.5-4B-Chat,28.75,4
+Qwen/Qwen1.5-1.8B-Chat,24.12,1.8
+Qwen/Qwen1.5-72B-Chat,82.81,72
+Qwen/Qwen1.5-7B-Chat,54.41,7
+vilm/Quyen-Pro-Max-v0.1,77.16,72
+ShinojiResearch/Senku-70B-Full,84.89,70`;
+
+const leaderboardDataMagi = `model,score
+YeungNLP/firefly-mixtral-8x7b,45.41
+macadeliccc/SOLAR-10.7b-Instruct-dpo,46.77
+meta-llama/Llama-2-70b-chat-hf,39.9
+mistralai/mistral-7b-instruct-v0.1,37.04
+Open-Orca/Mistral-7B-OpenOrca,39.18
+mlabonne/NeuralHermes-2.5-Mistral-7B,42.59
+WizardLM/WizardLM-13B-V1.2,36.78
+Yhyu13/LMCocktail-10.7B-v1,47.1
+Qwen/Qwen-1_8B-Chat,37.11
+Qwen/Qwen-72B-Chat,60.27
+Qwen/Qwen1.5-4B-Chat,38.92
+openchat/openchat-3.5-1210,43.46
+alpindale/goliath-120b,53.03
+ShinojiResearch/Senku-70B-Full,65.49
+Qwen/Qwen1.5-72B-Chat,63.35
+Qwen/Qwen-14B-Chat,45.35
+deepseek-ai/deepseek-llm-67b-chat,59.46
+OrionStarAI/Orion-14B-Chat,45.12
+meta-llama/Llama-2-7b-chat-hf,35.97
+NousResearch/Nous-Capybara-7B-V1,37.27
+01-ai/Yi-34B-Chat,58.22
+teknium/OpenHermes-2.5-Mistral-7B,42.36
+01-ai/Yi-6B-Chat,43.07
+miqudev/miqu-1-70b,66.72
+migtissera/Tess-XL-v1.0,50.63
+huggingfaceh4/zephyr-7b-alpha,39.9
+NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT,48.69
+allenai/tulu-2-dpo-70b,52.12
+madatnlp/marcoroni-7b-v3-safetensor,42.26
+cognitivecomputations/dolphin-2_2-yi-34b,60.88
+mlabonne/Beagle14-7B,44.05
+Toten5/Marcoroni-neural-chat-7B-v2,41.65
+berkeley-nest/Starling-LM-7B-alpha,43.04
+zyh3826/GML-Mistral-merged-v1,44.57
+Qwen/Qwen-7B-Chat,38.73
+lmsys/vicuna-13b-v1.5,36.56
+cognitivecomputations/dolphin-2.2-70b,51.77
+WizardLM/WizardLM-70B-V1.0,44.76
+stabilityai/stablelm-2-zephyr-1_6b,38.14
+cognitivecomputations/laserxtral,42.2
+mlabonne/NeuralMarcoro14-7B,42.95
+jondurbin/bagel-34b-v0.2,62.37
+lxuechen/phi-2-dpo,38.14
+Intel/neural-chat-7b-v3-1,41.42
+huggingfaceh4/zephyr-7b-beta,40.42
+jondurbin/nontoxic-bagel-34b-v0.2,65.97
+rhysjones/phi-2-orange,37.53
+timdettmers/guanaco-33b-merged,38.66
+DiscoResearch/DiscoLM-120b,55.08
+serpdotai/sparsetral-16x7B-v2,38.6
+NousResearch/Nous-Capybara-34B,61.47
+upstage/SOLAR-10.7B-Instruct-v1.0,46.93
+mistralai/Mistral-7B-Instruct-v0.2,39.9
+cloudyu/Mixtral_34Bx2_MoE_60B,63.31
+vilm/Quyen-Pro-Max-v0.1,60.4
+fblgit/una-cybertron-7b-v2-bf16,43.27
+Weyaxi/SauerkrautLM-UNA-SOLAR-Instruct,46.55
+lmsys/vicuna-7b-v1.1,41.36
+Qwen/Qwen1.5-1.8B-Chat,37.53
+lmsys/vicuna-33b-v1.3,38.37
+rishiraj/meow,46.71
+migtissera/SynthIA-70B-v1.5,50.86
+microsoft/phi-2,36.88
+openchat/openchat_3.5,42.2
+vince62s/phi-2-psy,37.24
+TheBloke/koala-7B-HF,20.08
+Qwen/Qwen1.5-14B-Chat,53.84
+mlabonne/Beyonder-4x7B-v2,43.04
+meta-llama/Llama-2-13b-chat-hf,37.17
+internlm/internlm2-chat-7b,41.94
+cognitivecomputations/MegaDolphin-120b,56.34
+mistralai/Mixtral-8x7B-Instruct-v0.1,49.56
+alpindale/miquella-120b,63.38
+mlabonne/phixtral-2x2_8,37.46
+Qwen/Qwen1.5-7B-Chat,48.17
+mlabonne/NeuralBeagle14-7B,44.21
+NousResearch/Nous-Hermes-2-Yi-34B,64.64
+cognitivecomputations/dolphin-2.2.1-mistral-7b,37.82
+gpt-3.5-turbo-0613,45.5
+gpt-3.5-turbo-1106,46.2
+gpt-3.5-turbo-0301,51.2
+gemini-pro,52.8
+mistral-medium,65.4`;
+
 function setupDarkModeToggle() {
 	var toggle = document.getElementById('darkModeToggle');
 	var label = document.getElementById('toggleLabel');
@@ -47,39 +220,179 @@ function decodeHtmlEntities(encodedString) {
 
 
 function loadLeaderboardData() {
-	fetch('leaderboard.csv?time=' + new Date().getTime())
-		.then(response => response.text())
-		.then(text => {
-			let rows = text.split('\n').filter(row => row.trim() !== '').slice(1);
-			let maxScore = Math.max(...rows.map(row => parseFloat(row.split(',')[1]))); // Get the highest score for reference
-			let html = rows.map(row => {
-				let [modelName, score, parameters] = row.split(',');
-				let scorePercentage = (parseFloat(score) / maxScore) * 100; // Calculate score as a percentage of maxScore
-				let modelNameDisplay = modelName.includes('/') 
-								? `<a href="https://huggingface.co/${modelName}" target="_blank">${modelName}</a>` 
-								: modelName;
-				let scoreBar = `<div class="score-bar-container">
-								<div class="score-bar" style="width: ${scorePercentage}%"></div>
-								<div class="score-text">${score}</div>
-							 </div>`;
-							 return `<tr><td>${modelNameDisplay}</td><td>${parameters}</td><td>${scoreBar}</td></tr>`;
-			}).join('');
-			document.getElementById('leaderboardBody').innerHTML = html;
-			initializeDataTable();              
-		});
+	const eqbenchRows = leaderboardDataEqbench.split('\n').slice(1); // Skip header for EQ-Bench data
+	const magiRows = leaderboardDataMagi.split('\n').slice(1).map(row => {
+		 const [model, score] = row.split(',');
+		 return { model, score: parseFloat(score) };
+	});
+
+	// Calculate max scores for each series
+	const maxScoreEQBench = Math.max(...eqbenchRows.map(row => parseFloat(row.split(',')[1])));
+	const maxScoreMagi = Math.max(...magiRows.map(row => row.score));
+
+	let html = eqbenchRows.map(eqbenchRow => {
+		 const [modelName, score, parameters] = eqbenchRow.split(',');
+		 const magiEntry = magiRows.find(magiRow => magiRow.model === modelName);
+		 const magiScore = magiEntry ? magiEntry.score : 0; // Use 0 if MAGI score is missing
+		 const scoreNum = parseFloat(score);
+		 const combined = magiScore ? ((scoreNum + magiScore) / 2).toFixed(2) : 0;
+		 
+		 // Calculate score percentages based on their respective max scores
+		 let scorePercentageEQ = (scoreNum / maxScoreEQBench) * 100;
+		 let scorePercentageMagi = magiEntry ? (magiScore / maxScoreMagi) * 100 : 0;
+
+		 let maxScoreCombined = Math.max(...eqbenchRows.map(row => {
+			  let score = parseFloat(row.split(',')[1]);
+			  let magiScore = magiRows.find(magiRow => magiRow.model === row.split(',')[0])?.score || 0;
+			  return magiScore ? ((score + magiScore) / 2) : 0;
+		 }));
+		 let scorePercentageCombined = ((parseFloat(combined) / maxScoreCombined) * 100) || 0;
+		 const modelNameDisplay = modelName.includes('/') 
+                        ? `<a href="https://huggingface.co/${modelName}" target="_blank">${modelName}</a>` 
+                        : modelName;
+		 
+		 let scoreBarEQ = `
+			<div class="score-bar-container">
+				<div class="score-bar" style="width: ${scorePercentageEQ}%"></div>
+				<span class="score-text">${score}</span>
+			</div>
+			`;		 
+		
+		let scoreBarMagi = magiEntry ? `<div class="score-bar-container">
+				<div class="score-bar" style="width: ${scorePercentageMagi}%"></div>		
+				<span class="score-text">${magiScore}</span>		
+		</div>
+		` : `<span class="score-text"></span>`;
+
+		let scoreBarCombined = combined ? `<div class="score-bar-container">
+		<div class="score-bar" style="width: ${scorePercentageCombined}%"></div>		
+		<span class="score-text">${combined}</span>
+		</div>
+		` : `<span class="score-text"></span>`;
+
+		 return `<tr>
+						 <td>${modelNameDisplay}</td>
+						 <td>${parameters}</td>
+						 <td data-order="${score}">${scoreBarEQ}</td>
+						 <td data-order="${magiScore}">${scoreBarMagi}</td>
+						 <td data-order="${combined}">${scoreBarCombined}</td>
+					</tr>`;
+	}).join('');
+	
+	document.getElementById('leaderboardBody').innerHTML = html;
+	initializeDataTable();
 }
 
+
 function initializeDataTable() {
-	$('#leaderboard').DataTable({
-		 "order": [[2, "desc"]],
+	let table = $('#leaderboard').DataTable({
+		 "order": [[4, "desc"]], // Default sorting
 		 "pageLength": 100,
 		 "lengthMenu": [50, 100, 200, 1000],
 		 "language": {
 			  "lengthMenu": "Show _MENU_"
 		 },
-		 "dom":
-			  "<'d-flex flex-column flex-md-row justify-content-between'<'dataTables_length'l><'dataTables_filter'f>>" +
-			  "<'row'<'col-12'tr>>" +
-			  "<'row'<'col-md-5'i><'col-md-7'p>>",
-	});
+		 "columnDefs": [
+				{ "targets": [2, 3, 4], "orderSequence": ["desc", "asc"] }, // For score columns: sort desc first
+				{
+					"targets": [3,4], // Index of the MAGI & Combined columns
+					"type": "your-custom-sort"
+				},
+		],
+		 "dom": "<'d-flex flex-column flex-md-row justify-content-between'<'dataTables_length'l><'dataTables_filter'f>>" +
+				  "<'row'<'col-12'tr>>" +
+				  "<'row'<'col-md-5'i><'col-md-7'p>>",
+		"drawCallback": function(settings) {
+					// Hide all score bars initially
+					$('.score-bar').hide();
+			  
+					let api = this.api();
+					let sortedColumnIndex = api.order()[0][0];
+					const SCORE_COLUMNS = [2, 3, 4]; // Indices for the score columns (EQ-Bench, MAGI, and Avg)
+					const MODEL_PARAMS_COLUMNS = [0, 1]; // Indices for the Model and Params columns
+			  
+					// Check if the sorted column is a score column
+					if (SCORE_COLUMNS.includes(sortedColumnIndex)) {
+						 // Show score bar for the sorted score column only
+						 api.cells(null, sortedColumnIndex).nodes().to$().find('.score-bar').show();
+			  
+						 // Adjust the width of the sorted score column
+						 $('th').css('width', ''); // Reset widths for all headers
+						 $(api.column(sortedColumnIndex).header()).css('width', '33%');
+			  
+						 // Update the last sorted score column
+						 lastSortedScoreColumn = sortedColumnIndex;
+					} else if (MODEL_PARAMS_COLUMNS.includes(sortedColumnIndex)) {
+						 // If sorted by Model or Params, do not reset the score bars and column width
+						 if (lastSortedScoreColumn !== null) {
+							  // Reapply the width adjustment to the last sorted score column
+							  $('th').css('width', '');
+							  $(api.column(lastSortedScoreColumn).header()).css('width', '33%');
+			  
+							  // Make the score bar of the last sorted score column visible
+							  api.cells(null, lastSortedScoreColumn).nodes().to$().find('.score-bar').show();
+						 }
+					}
+			  }
+    });
 }
+
+
+
+let lastSortedScoreColumn = null;
+
+function adjustScoreBarsAndColumnWidth(table, sortedColumnIndex) {
+    const SCORE_COLUMNS = [2, 3, 4];
+    const MODEL_PARAMS_COLUMNS = [0, 1];
+
+    // Reset width adjustments for all columns
+    $('th').css('width', '');
+
+    // Determine action based on the sorted column
+    if (SCORE_COLUMNS.includes(sortedColumnIndex)) {
+        // Sorting by a score column
+        // Show the score bar for the sorted score column
+        $('.score-bar').css('display', 'none');
+        table.columns(sortedColumnIndex).nodes().flatten().to$().find('.score-bar').css('display', 'block');
+        
+        $(table.column(sortedColumnIndex).header()).css('width', '33%');
+
+        lastSortedScoreColumn = sortedColumnIndex;
+    } else if (MODEL_PARAMS_COLUMNS.includes(sortedColumnIndex)) {
+        // Sorting by Model or Params column
+        // Do not adjust width, but maintain the state of score bars
+        if (lastSortedScoreColumn !== null) {
+            $('.score-bar').css('display', 'none');
+            table.columns(lastSortedScoreColumn).nodes().flatten().to$().find('.score-bar').css('display', 'block');
+        }
+    }
+}
+
+
+$.fn.dataTable.ext.type.order['your-custom-sort'] = function (data) {
+	// Handle missing or null values;
+	if (!data || data === '-') {
+		 return -1; // Always sort these values last
+	}
+	return parseFloat(data) || 0; // Convert to float for sorting, defaulting to 0
+};
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	const expandoBtn = document.getElementById('expando-btn');
+	const expandoContent = document.querySelector('.expando-content');
+	expandoContent.style.display = 'none';
+	expandoBtn.textContent = 'Click to show citations';
+
+	expandoBtn.addEventListener('click', function() {
+		if (expandoContent.style.display === 'none' || expandoContent.style.display === '') {
+			 expandoContent.style.display = 'block';
+			 expandoBtn.textContent = 'Click to hide citations';
+			 // Scroll the expandoContent into view
+			 expandoContent.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		} else {
+			 expandoContent.style.display = 'none';
+			 expandoBtn.textContent = 'Click to show citations';
+		}
+  });
+});
