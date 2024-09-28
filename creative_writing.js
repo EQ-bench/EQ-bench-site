bec9c163c06ed8ccddc32fabc7c7cb4340d85212
sqrkl <152407511+sqrkl@users.noreply.github.com>
2024-09-28 22:27:49 +1000

    add gpt slop metric

--- creative_writing.js
+++ creative_writing.js
@@ -1,64 +1,64 @@
 let leaderboardDataCreativeWriting = `model,score,ci95_low,ci95_high,params,avgLength
-Qwen/Qwen1.5-4B-Chat,27.01,26.48,27.54,4,3219
-claude-3-5-sonnet-20240620,78.77,78.41,79.13,,5875
-claude-3-haiku-20240307,68.97,68.14,69.81,,4907
-claude-3-opus-20240229,73.58,72.51,74.65,,4846
-gpt-4o,75.61,74.87,76.34,,5401
-gemini-1.5-pro-001,80.27,79.76,80.77,,4312
-alpindale/WizardLM-2-8x22B,78.91,78.17,79.65,8x22,5856
-meta-llama/Llama-3-70b-chat-hf,71.28,70.07,72.48,70,4439
-01-ai/Yi-34B-Chat,71.1,69.75,72.44,34,4496
-Qwen/Qwen1.5-14B-Chat,65.23,64.22,66.25,14,3736
-mistralai/Mixtral-8x7B-Instruct-v0.1,59.32,58.16,60.47,8x7,4775
-meta-llama/Llama-3-8b-chat-hf,61.7,60.38,63.02,8,4112
-meta-llama/Llama-2-13b-chat-hf,49.14,47.87,50.4,13,3809
-google/gemma-7b-it,46.19,44.94,47.44,7,2671
-Qwen/Qwen1.5-110B-Chat,75.33,74.55,76.12,110,4269
-google/gemma-2b-it,41.13,40.23,42.03,2,2890
-meta-llama/Llama-2-7b-chat-hf,46.67,45.32,48.01,7,3874
-garage-bAInd/Platypus2-70B-instruct,47.45,45.8,49.09,70,3561
-openchat/openchat-3.5-1210,57.28,56.38,58.17,7,4914
-mistralai/Mixtral-8x22B-Instruct-v0.1,63.64,62.49,64.8,8x22,4390
-gemini-1.5-flash-001,71.96,71.08,72.84,,4933
-!sophosympatheia/Midnight-Miqu-70B-v1.5,77.15,76.62,77.68,70,7787
-CohereForAI/c4ai-command-r-plus,64.75,63.95,65.55,104,5961
-gpt-4-0125-preview,77.43,76.84,78.01,,5047
-gpt-3.5-turbo-0301,42.33,41.2,43.47,,3443
-!Sao10K/L3-70B-Euryale-v2.1,67.32,66.35,68.28,70,4473
-OmnicromsBrain/NeuralStar_AlphaWriter_4x7b,74.21,73.61,74.82,4x7,5461
-!Sao10K/L3-8B-Stheno-v3.2,69.21,68.62,69.8,8,4321
-google/gemma-2-9b-it,76.15,75.13,77.17,9,4010
-google/gemma-2-27b-it,77.22,76.18,78.24,27,4010
-!TheDrummer/Llama-3SOME-8B-v2,70.59,69.54,71.65,8,4466
-!sophosympatheia/New-Dawn-Llama-3-70B-32K-v1.0,58.48,54.75,62.22,70,6811
-N8Programs/Coxcomb,71.4,70.62,72.17,7,5474
-UCLA-AGI/Gemma-2-9B-It-SPPO-Iter3,78.76,78.17,79.36,9,4210
-gpt-4o-mini,78.41,77.62,79.21,,5647
-DeepSeek-V2-Chat-0628,69.77,68.84,70.69,236B-21A,5489
-mistralai/Mistral-Nemo-Instruct-2407,77.46,76.82,78.1,12,6265
-mistralai/Mistral-Large-Instruct-2407,77.17,75.3,79.03,123,4789
-gemini-1.5-pro-exp-0801,80.34,79.7,80.99,,5329
-meta-llama/Meta-Llama-3.1-405B-Instruct,71.98,71.15,72.81,405,4462
-dnhkng/RYS-Llama-3-Huge-Instruct,75.63,75.2,76.06,99,4507
-OmnicromsBrain/NeuralStar_FusionWriter_4x7b,75.09,73.26,76.92,4x7,4956
-!OmnicromsBrain/Eros_Scribe-7b,68.04,66.54,69.53,7,10040
-lemon07r/Gemma-2-Ataraxy-9B,82.36,81.97,82.76,9,4574
-nbeerbower/mistral-nemo-gutenberg-12B-v2,80.12,79.2,81.03,12,4988
-chatgpt-4o-latest-2024-08-08,81.36,80.97,81.74,,6784
-ArliAI/Mistral-Nemo-12B-ArliAI-RPMax-v1.1,56.42,53.63,59.2,12,5419
-!FluffyKaeloky/Luminum-v0.1-123B,67.6,67.6,67.6,123,4963
-!TheDrummer/Theia-21B-v1,58.87,58.26,59.49,21,6265
-CohereForAI/c4ai-command-r-08-2024,65.67,62.67,68.67,32,4521
-!Sao10K/MN-12B-Lyra-v1,72.85,70.8,74.9,12,5974
-arcee-ai/Arcee-Scribe,64.15,62.15,66.14,7,5016
-ifable/gemma-2-Ifable-9B,84.59,84.24,84.95,9,4936
-o1-mini-2024-09-12,76.32,74.43,78.21,,6193
-o1-preview-2024-09-12,80.52,80.31,80.74,,7072
-CohereForAI/c4ai-command-r-plus-08-2024,76.13,75.34,76.92,104,5566
-chatgpt-4o-latest-2024-09-03,82.51,82.04,82.99,,6204
-mistralai/Mistral-Small-Instruct-2409,72.39,70.03,74.75,22,5914
-*gemini-1.5-pro-002,81.44,81.13,81.76,,4799
-*Qwen/Qwen2.5-72B-Instruct,72.16,70.06,74.26,72,7344`
+Qwen/Qwen1.5-4B-Chat,27.01,26.48,27.54,4,3219,4.5
+claude-3-5-sonnet-20240620,78.77,78.41,79.13,,5875,5.45
+claude-3-haiku-20240307,68.97,68.14,69.81,,4907,6.41
+claude-3-opus-20240229,73.58,72.51,74.65,,4846,4.47
+gpt-4o,75.61,74.87,76.34,,5401,8.5
+gemini-1.5-pro-001,80.27,79.76,80.77,,4312,6.79
+alpindale/WizardLM-2-8x22B,78.91,78.17,79.65,8x22,5856,9.0
+meta-llama/Llama-3-70b-chat-hf,71.28,70.07,72.48,70,4439,6.91
+01-ai/Yi-34B-Chat,71.1,69.75,72.44,34,4496,8.86
+Qwen/Qwen1.5-14B-Chat,65.23,64.22,66.25,14,3736,13.86
+mistralai/Mixtral-8x7B-Instruct-v0.1,59.32,58.16,60.47,8x7,4775,9.09
+meta-llama/Llama-3-8b-chat-hf,61.7,60.38,63.02,8,4112,8.02
+meta-llama/Llama-2-13b-chat-hf,49.14,47.87,50.4,13,3809,6.85
+google/gemma-7b-it,46.19,44.94,47.44,7,2671,14.24
+Qwen/Qwen1.5-110B-Chat,75.33,74.55,76.12,110,4269,12.47
+google/gemma-2b-it,41.13,40.23,42.03,2,2890,15.95
+meta-llama/Llama-2-7b-chat-hf,46.67,45.32,48.01,7,3874,8.21
+garage-bAInd/Platypus2-70B-instruct,47.45,45.8,49.09,70,3561,6.54
+openchat/openchat-3.5-1210,57.28,56.38,58.17,7,4914,8.76
+mistralai/Mixtral-8x22B-Instruct-v0.1,63.64,62.49,64.8,8x22,4390,9.78
+gemini-1.5-flash-001,71.96,71.08,72.84,,4933,9.09
+!sophosympatheia/Midnight-Miqu-70B-v1.5,77.15,76.62,77.68,70,7787,6.28
+CohereForAI/c4ai-command-r-plus,64.75,63.95,65.55,104,5961,7.62
+gpt-4-0125-preview,77.43,76.84,78.01,,5047,9.84
+gpt-3.5-turbo-0301,42.33,41.2,43.47,,3443,5.56
+!Sao10K/L3-70B-Euryale-v2.1,67.32,66.35,68.28,70,4473,4.11
+OmnicromsBrain/NeuralStar_AlphaWriter_4x7b,74.21,73.61,74.82,4x7,5461,11.5
+!Sao10K/L3-8B-Stheno-v3.2,69.21,68.62,69.8,8,4321,8.18
+google/gemma-2-9b-it,76.15,75.13,77.17,9,4010,12.10
+google/gemma-2-27b-it,77.22,76.18,78.24,27,4010,9.75
+!TheDrummer/Llama-3SOME-8B-v2,70.59,69.54,71.65,8,4466,6.85
+!sophosympatheia/New-Dawn-Llama-3-70B-32K-v1.0,58.48,54.75,62.22,70,6811,6.48
+N8Programs/Coxcomb,71.4,70.62,72.17,7,5474,8.26
+UCLA-AGI/Gemma-2-9B-It-SPPO-Iter3,78.76,78.17,79.36,9,4210,9.06
+gpt-4o-mini,78.41,77.62,79.21,,5647,7.27
+DeepSeek-V2-Chat-0628,69.77,68.84,70.69,236B-21A,5489,7.75
+mistralai/Mistral-Nemo-Instruct-2407,77.46,76.82,78.1,12,6265,7.22
+mistralai/Mistral-Large-Instruct-2407,77.17,75.3,79.03,123,4789,6.79
+gemini-1.5-pro-exp-0801,80.34,79.7,80.99,,5329,7.4
+meta-llama/Meta-Llama-3.1-405B-Instruct,71.98,71.15,72.81,405,4462,7.14
+dnhkng/RYS-Llama-3-Huge-Instruct,75.63,75.2,76.06,99,4507,6.5
+OmnicromsBrain/NeuralStar_FusionWriter_4x7b,75.09,73.26,76.92,4x7,4956,11.77
+!OmnicromsBrain/Eros_Scribe-7b,68.04,66.54,69.53,7,10040,6.37
+lemon07r/Gemma-2-Ataraxy-9B,82.36,81.97,82.76,9,4574,10.2
+nbeerbower/mistral-nemo-gutenberg-12B-v2,80.12,79.2,81.03,12,4988,12.92
+chatgpt-4o-latest-2024-08-08,81.36,80.97,81.74,,6784,5.15
+ArliAI/Mistral-Nemo-12B-ArliAI-RPMax-v1.1,56.42,53.63,59.2,12,5419,4.31
+!FluffyKaeloky/Luminum-v0.1-123B,67.6,67.6,67.6,123,4963,5.65
+!TheDrummer/Theia-21B-v1,58.87,58.26,59.49,21,6265,6.23
+CohereForAI/c4ai-command-r-08-2024,65.67,62.67,68.67,32,4521,11.3
+!Sao10K/MN-12B-Lyra-v1,72.85,70.8,74.9,12,5974,4.48
+arcee-ai/Arcee-Scribe,64.15,62.15,66.14,7,5016,11.79
+ifable/gemma-2-Ifable-9B,84.59,84.24,84.95,9,4936,12.65
+o1-mini-2024-09-12,76.32,74.43,78.21,,6193,8.77
+o1-preview-2024-09-12,80.52,80.31,80.74,,7072,6.99
+CohereForAI/c4ai-command-r-plus-08-2024,76.13,75.34,76.92,104,5566,9.53
+chatgpt-4o-latest-2024-09-03,82.51,82.04,82.99,,6204,4.78
+mistralai/Mistral-Small-Instruct-2409,72.39,70.03,74.75,22,5914,7.05
+*gemini-1.5-pro-002,81.44,81.13,81.76,,4799,6.64
+*Qwen/Qwen2.5-72B-Instruct,72.16,70.06,74.26,72,7344,5.42`
 
 function setupDarkModeToggle() {
 	var toggle = document.getElementById('darkModeToggle');
@@ -144,10 +144,10 @@ function loadLeaderboardData() {
 	// Calculate max score for Creative Writing
 	maxScoreCreativeWriting = Math.max(...creativeWritingRows.map(row => parseFloat(row.split(',')[1])));
 
-	const maxScore = Math.max(...creativeWritingRows.map(row => parseFloat(row.split(',')[1])));
+	const maxScore = maxScoreCreativeWriting;
 
 	let html = creativeWritingRows.map(creativeWritingRow => {
-		let [modelName, score, ci95_low, ci95_high, params, avgLength] = creativeWritingRow.split(',');
+		let [modelName, score, ci95_low, ci95_high, params, avgLength, gptSlop] = creativeWritingRow.split(',');
 		const scoreNum = parseFloat(score);
 		const ci95LowNum = parseFloat(ci95_low);
 		const ci95HighNum = parseFloat(ci95_high);
@@ -157,10 +157,6 @@ function loadLeaderboardData() {
 		const errorBarLeftPos = ((ci95LowNum / maxScore) * 100).toFixed(2);
 		const errorBarRightPos = ((ci95HighNum / maxScore) * 100).toFixed(2);
 		const errorBarWidth = (errorBarRightPos - errorBarLeftPos).toFixed(2);
-
-		console.log(maxScore, ci95HighNum)
-		console.log(errorBarLeftPos, errorBarRightPos, errorBarWidth)
-
 
 		const isNsfwModel = modelName.startsWith('!');
 		modelName = modelName.replace(/^\!/, '');
@@ -193,6 +189,7 @@ function loadLeaderboardData() {
             <td>${modelNameDisplay}</td>
             <td>${params}</td>
             <td>${avgLength}</td>
+            <td>${gptSlop}</td>
             <td data-order="${score}">
                 ${scoreBarCreativeWriting}
             </td>
@@ -205,7 +202,7 @@ function loadLeaderboardData() {
 	updateCreativeWritingScoreBarColors();
 }
 
-let currentOrder = [[3, "desc"]]; // Default sorting
+let currentOrder = [[4, "desc"]]; // Default sorting
 let dataTableConfig = {
 	"order": currentOrder,
 	"paging": false, // Disable pagination
@@ -215,8 +212,8 @@ let dataTableConfig = {
 	  "lengthMenu": "Show _MENU_"
 	},
 	"columnDefs": [
-	  { "targets": [3], "orderSequence": ["desc", "asc"] },
-	  { "targets": [2], "orderSequence": ["desc", "asc"] },
+	  { "targets": [4], "orderSequence": ["desc", "asc"] },
+	  { "targets": [2,3], "orderSequence": ["desc", "asc"] },
 	  { "targets": [1], "type": "params", "orderSequence": ["asc", "desc"] },
 	],
 	"dom": "<'d-flex flex-column flex-md-row justify-content-between'<'dataTables_length'l><'dataTables_filter'f>>" +
@@ -231,18 +228,14 @@ function initializeDataTable() {
 	let table = $('#leaderboard').DataTable(dataTableConfig);
 }
 
-let lastSortedScoreColumn = null;
-
 document.addEventListener('DOMContentLoaded', function() {
 	// Always execute
 	displayEncodedEmail();
-	//setupDarkModeToggle();
-	//applySystemTheme();
 
 	// Conditional execution based on the presence of elements
 	if (document.getElementById('leaderboard')) {
 		loadLeaderboardData(); // Only load leaderboard data if the leaderboard element exists
-		setupLengthControl();
+		setupControls();
 	}
 
 	// This checks if the system theme preference should be applied, which is common functionality
@@ -250,25 +243,41 @@ document.addEventListener('DOMContentLoaded', function() {
 	setupDarkModeToggle();
 });
 
-function setupLengthControl() {
-	const slider = document.getElementById('lengthControlSlider');
-	const sliderValueLabel = document.getElementById('lengthControlValue');
- 
-	// Set the initial value of the slider value label
-	sliderValueLabel.textContent = `${slider.value}%`;
- 
-	// Update scores based on the initial slider value
-	updateScoresBasedOnLengthControl(slider.value);
- 
-	slider.oninput = function() {
-	  sliderValueLabel.textContent = `${this.value}%`;
-	  updateScoresBasedOnLengthControl(this.value);
-	};
- }
+function setupControls() {
+	const lengthSlider = document.getElementById('lengthControlSlider');
+	const lengthSliderValueLabel = document.getElementById('lengthControlValue');
 
- function updateScoresBasedOnLengthControl(percentage) {
-	const adjustmentFactor = 0.1 * parseFloat(percentage) / 100;
+	const gptSlopSlider = document.getElementById('gptSlopControlSlider');
+	const gptSlopSliderValueLabel = document.getElementById('gptSlopControlValue');
+
+	// Set the initial value of the slider value labels
+	lengthSliderValueLabel.textContent = `${lengthSlider.value}%`;
+	gptSlopSliderValueLabel.textContent = `${gptSlopSlider.value}%`;
+
+	// Update scores based on the initial slider values
+	updateScores();
+
+	lengthSlider.oninput = function() {
+		lengthSliderValueLabel.textContent = `${this.value}%`;
+		updateScores();
+	};
+
+	gptSlopSlider.oninput = function() {
+		gptSlopSliderValueLabel.textContent = `${this.value}%`;
+		updateScores();
+	};
+}
+
+function updateScores() {
+	const lengthPercentage = document.getElementById('lengthControlSlider').value;
+	const gptSlopPercentage = document.getElementById('gptSlopControlSlider').value;
+
+	const lengthAdjustmentFactor = 0.1 * parseFloat(lengthPercentage) / 100;
+	const gptSlopAdjustmentFactor = 0.1 * parseFloat(gptSlopPercentage) / 100;
+
 	const avgLength = calculateAverageLength();
+	const avgGptSlop = calculateAverageGptSlop();
+
 	const table = $('#leaderboard').DataTable();
 
 	// Get the current page and sorting information
@@ -282,22 +291,29 @@ function setupLengthControl() {
 	$('#leaderboardBody tr').each(function() {
 		 const row = $(this);
 		 const avgModelLength = parseFloat(row.find('td:eq(2)').text());
+		 const modelGptSlop = parseFloat(row.find('td:eq(3)').text());
 		 const originalScore = parseFloat(row.attr('data-original-score'));
 		 const originalCILow = parseFloat(row.attr('data-original-ci-low'));
 		 const originalCIHigh = parseFloat(row.attr('data-original-ci-high'));
 
-		 let lengthAdjustmentFactor = avgLength / avgModelLength * adjustmentFactor + 1 - adjustmentFactor;
-		 if (lengthAdjustmentFactor > 1.15) { lengthAdjustmentFactor = 1.15 }
-		 if (lengthAdjustmentFactor < 0.85) { lengthAdjustmentFactor = 0.85 }
+		 // Length adjustment
+		 let lengthAdjustment = avgLength / avgModelLength * lengthAdjustmentFactor + 1 - lengthAdjustmentFactor;
+		 if (lengthAdjustment > 1.15) { lengthAdjustment = 1.15 }
+		 if (lengthAdjustment < 0.85) { lengthAdjustment = 0.85 }
 
-		 const adjustedScore = originalScore * lengthAdjustmentFactor;
-		 const adjustedCILow = adjustedScore - (originalScore - originalCILow) //originalCILow * lengthAdjustmentFactor;
-		 const adjustedCIHigh = adjustedScore + (originalCIHigh - originalScore) //originalCIHigh * lengthAdjustmentFactor;
+		 // GPT-Slop adjustment
+		 let gptSlopAdjustment = avgGptSlop / modelGptSlop * gptSlopAdjustmentFactor + 1 - gptSlopAdjustmentFactor;
+		 if (gptSlopAdjustment > 1.15) { gptSlopAdjustment = 1.15 }
+		 if (gptSlopAdjustment < 0.85) { gptSlopAdjustment = 0.85 }
+
+		 const adjustedScore = originalScore * lengthAdjustment * gptSlopAdjustment;
+		 const adjustedCILow = adjustedScore - (originalScore - originalCILow) * lengthAdjustment * gptSlopAdjustment;
+		 const adjustedCIHigh = adjustedScore + (originalCIHigh - originalScore) * lengthAdjustment * gptSlopAdjustment;
 
 		 const scoreText = adjustedScore.toFixed(2);
 
 		 // Update the score
-		 row.find('td:eq(3)').attr('data-order', scoreText).find('.score-text').text(scoreText);
+		 row.find('td:eq(4)').attr('data-order', scoreText).find('.score-text').text(scoreText);
 
 		 // Update error bar position and width
 		 const errorBarLeftPos = ((adjustedCILow / maxScoreCreativeWriting) * 98).toFixed(2);
@@ -319,17 +335,20 @@ function setupLengthControl() {
 	// Update the score bar widths
 	$('#leaderboardBody tr').each(function() {
 		 const row = $(this);
-		 const scoreText = row.find('td:eq(3)').attr('data-order');
+		 const scoreText = row.find('td:eq(4)').attr('data-order');
 		 const percentageWidth = Math.max(0, Math.min(100, (parseFloat(scoreText) / maxScoreCreativeWriting) * 98));
 		 row.find('.creative-writing-score-bar').css('width', `${percentageWidth}%`);
 	});
 }
 
-
-
-
 function calculateAverageLength() {
 	const lengths = Array.from(document.querySelectorAll('#leaderboardBody tr'))
 		 .map(row => parseFloat(row.cells[2].textContent));
 	return lengths.reduce((a, b) => a + b, 0) / lengths.length;
+}
+
+function calculateAverageGptSlop() {
+	const gptSlops = Array.from(document.querySelectorAll('#leaderboardBody tr'))
+		 .map(row => parseFloat(row.cells[3].textContent));
+	return gptSlops.reduce((a, b) => a + b, 0) / gptSlops.length;
 }
